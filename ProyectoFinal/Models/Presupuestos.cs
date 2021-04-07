153689ab4d15af617197cf2cd0bdb211646625e9
Wilbert Smith Antigua Castillo <wilbert_antigua@ucne.edu.do>
2021-04-06 21:27:49 -0500

    presupuesto y Usuarios enlazado

--- ProyectoFinal/Models/Presupuestos.cs
+++ ProyectoFinal/Models/Presupuestos.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,13 +12,24 @@ namespace ProyectoFinal.Models
         [Key]
         [Required(ErrorMessage = "El campo Id no puede estar vacío.")]
         public int PresupuestoId { get; set; }
+
         public int CategoriaId { get; set; }
+
         [Required(ErrorMessage = "El campo Categoria no puede estar vacío.")]
         public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "Este campo no puede estar vacío.")]
+        public int UsuarioId { get; set; }
+
         [Required(ErrorMessage = "El campo Monto no puede estar vacío.")]
         public decimal Monto { get; set; }
+
         [Required(ErrorMessage = "El campo Fecha no puede estar vacío.")]
         public DateTime Fecha { get; set; }
+
+        [ForeignKey("UsuarioId")]
+        public virtual Usuarios Usuarios { get; set; }
+
         public virtual List<PresupuestoDetalle> Detalle { get; set; }
 
         public Presupuestos()
